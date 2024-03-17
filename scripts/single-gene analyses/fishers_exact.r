5bdb8dce3f19e58cb3189189ef3b7b85b7adb470
antonatosc <156329470+antonatosc@users.noreply.github.com>
2024-03-17 17:18:29 +0200

    Update fishers_exact.r

--- scripts/single-gene analyses/fishers_exact.r
+++ scripts/single-gene analyses/fishers_exact.r
@@ -3,6 +3,6 @@ gene_overlap_test = function(gene_list1, gene_list2, background){
   B = length(gene_list2)
   C = background
   AB = length(intersect(gene_list1, gene_list2))
-  pval = phyper(C-1, B, C-B, A, lower.tail = FALSE)
-  writeLines(paste0("Length of list A: ", A, ".\nLength of list B: ", B, ".\n", AB, " mathcing from lists A and B.\nP-value:",pval))
+  pval = phyper(AB-1, B, C-B, A, lower.tail = FALSE)
+  writeLines(paste0("Length of list A: ", A, ".\nLength of list B: ", B, ".\n", AB, " matching from lists A and B.\nP-value:",pval)) #typo
 }
